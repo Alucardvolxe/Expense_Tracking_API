4e66aa0fea227d86a3db24ce2abc97fea959d79d
Daniel Nortey <gogamerdan247@gmail.com>
2025-11-17 22:09:15 +0000

    starting on login and logout

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     ## installed app
-    'Api',
+    'Accounts',
     'rest_framework',
     'debug_toolbar',
 ]
@@ -131,3 +131,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
